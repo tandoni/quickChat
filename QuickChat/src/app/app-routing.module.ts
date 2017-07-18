912ae72de2029a8a9bf649050414da6fee3321b0
Ishank Tandon <IshankTandon@ishank.wlan.rose-hulman.edu>
2017-07-18 12:15:30 -0400

    routing + placeholder sidenav

--- QuickChat/src/app/app-routing.module.ts
+++ QuickChat/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from "app/+main/main.component";
+import { SignInComponent } from "app/+sign-in/sign-in.component";
+import { MyPostsComponent } from "app/+my-posts/my-posts.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', pathMatch: 'full', component: MainComponent },
+  { path: 'signin', component: SignInComponent },
+  { path: 'myposts', component: MyPostsComponent},
+  { path: '**', redirectTo: '/' },
 ];
 
 @NgModule({
